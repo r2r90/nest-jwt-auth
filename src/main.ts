8648bba41aa585ffdd04213a40abfb01b1353de5
Artur Agh <aghartur@gmail.com>
2025-06-16 11:41:39 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('JWT AUTH NESTJS')
+        .setDescription('API Documentation for NestJs JWT AUTH')
+        .setVersion('1.0.0')
+        .setContact(
+            'r2r90 Team',
+            'https://github.com/r2r90',
+            'aghartur@gmail.com',
+        )
+        .build();
+
+
+    const document = SwaggerModule.createDocument(app, config)
+
+    SwaggerModule.setup('api/docs', app, document, {
+        jsonDocumentUrl: '/swagger.json',
+        yamlDocumentUrl: '/swagger.yaml',
+    });
+
+    await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
